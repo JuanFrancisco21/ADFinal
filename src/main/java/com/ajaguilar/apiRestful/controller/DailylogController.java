fe9b0499fdd1a716186d2372547d27b78c23c958
adry29 <adrycrack@gmail.com>
2022-02-07 18:16:41 +0100

    Controller y servicio dailylog comentado

--- src/main/java/com/ajaguilar/apiRestful/controller/DailylogController.java
+++ src/main/java/com/ajaguilar/apiRestful/controller/DailylogController.java
@@ -4,9 +4,11 @@ import com.ajaguilar.apiRestful.model.Dailylog;
 import com.ajaguilar.apiRestful.services.DailylogService;
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,45 +31,105 @@ public class DailylogController {
     @Autowired
     DailylogService service;
     
+    /**
+     * Método que devuelve una lista con todos los dailylogs de la BD
+     * @return una List con los dailylogs
+    */
     @GetMapping
     public ResponseEntity<List<Dailylog>> getAllDailylogs(){
-        List<Dailylog> result = service.getAllDailylogs();
-        return new ResponseEntity<List<Dailylog>>(result, new HttpHeaders(), HttpStatus.OK);
+        try{
+            List<Dailylog> result = service.getAllDailylogs();
+            return new ResponseEntity<List<Dailylog>>(result, new HttpHeaders(), HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<List<Dailylog>>(new ArrayList<Dailylog>(), new HttpHeaders(), HttpStatus.NOT_FOUND);
+        }
     }
     
+    /**
+     * Método para obtener un dailylog concreto según su id
+     * @param id del dailylog a devolver
+     * @return el objeto dailylog en caso de que este exista, un dailylog
+     * vac?o en caso contrario
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Dailylog> getDailylogById(@PathVariable("id") Long id){
-        Dailylog result = service.getDailylogbyId(id);
-        return new ResponseEntity<Dailylog>(result, new HttpHeaders(), HttpStatus.OK);
+        try{
+            Dailylog result = service.getDailylogbyId(id);
+            return new ResponseEntity<Dailylog>(result, new HttpHeaders(), HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<Dailylog>(new Dailylog(), new HttpHeaders(), HttpStatus.NOT_FOUND);
+        }
     }
     
+    /**
+     * Método que crea un dailylog en la BD
+     * @param log Dailylog a crear
+     * @return El dailylog creado si tiene éxito, dailylog vacío si falla
+     */
     @PostMapping
-    public ResponseEntity<Dailylog> createDailylog(Dailylog log){
-        Dailylog result = service.createDailylog(log);
-        return new ResponseEntity<Dailylog>(result, new HttpHeaders(), HttpStatus.OK);
+    public ResponseEntity<Dailylog> createDailylog(@Valid @RequestBody Dailylog log){
+        if(log != null && log.getId()==-1){
+            try{
+                Dailylog result = service.createDailylog(log);
+                return new ResponseEntity<Dailylog>(result, new HttpHeaders(), HttpStatus.OK);
+            }catch(Exception e){
+                return new ResponseEntity<Dailylog>(new Dailylog(), new HttpHeaders(), HttpStatus.NOT_FOUND);
+            }
+        }else{
+            return new ResponseEntity<Dailylog>(new Dailylog(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+        }
     }
     
+    /**
+     * Método para editar un dailylog que ya exista en la BD
+     * @param log Dailylog a editar
+     * @return El objeto dailylog editado
+     */
     @PutMapping
-    public ResponseEntity<Dailylog> updateDailylog(Dailylog log){
-        Dailylog result = new Dailylog();
+    public ResponseEntity<Dailylog> updateDailylog(@Valid @RequestBody Dailylog log){
+        Dailylog result;
         try {
             result = service.updateDailylog(log);
+            return new ResponseEntity<Dailylog>(result, new HttpHeaders(), HttpStatus.OK);
         } catch (Exception ex) {
-            Logger.getLogger(DailylogController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<Dailylog>(new Dailylog(), new HttpHeaders(), HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<Dailylog>(result, new HttpHeaders(), HttpStatus.OK);
+        
     }
     
+    /**
+     * Método que elimina un dailylog de la BD según su id
+     * @param id ID del dailylog que se busca eliminar
+     * @return Status ok si el objeto es eliminado, Bad Request en caso contrario
+     */
     @DeleteMapping("/{id}")
     public HttpStatus deleteDailylogById(@PathVariable("id") Long id){
-        service.deleteDailylog(id);
-        return HttpStatus.OK;
+        if(id > 0){
+            try{
+                service.deleteDailylog(id);
+                return HttpStatus.OK;
+            }catch(Exception e){
+                return HttpStatus.NOT_FOUND;
+            }
+        }else{
+            return HttpStatus.BAD_REQUEST;
+        }
     }
     
+    /**
+     * Método que devuelve todos los dailylogs creados en una fecha concreta
+     * @param date Fecha de los dailylogs
+     * @return Lista de los dailylogs que coincidan con dicha fecha
+     */
     @GetMapping("/{date}")
     public ResponseEntity<List<Dailylog>> getDailylogsByDate(@PathVariable("date") LocalDate date){
         Date day = Date.valueOf(date);
-        List<Dailylog> result = service.getDailylogsByDay(day);
-        return new ResponseEntity<List<Dailylog>>(result, new HttpHeaders(), HttpStatus.OK);
-    }    
-}
+        try{
+            List<Dailylog> result = service.getDailylogsByDay(day);
+            return new ResponseEntity<List<Dailylog>>(result, new HttpHeaders(), HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<List<Dailylog>>(new ArrayList<Dailylog>(), new HttpHeaders(), HttpStatus.NOT_FOUND);
+        }
+        
+    }
+}    
--- src/main/java/com/ajaguilar/apiRestful/repository/DailylogRepository.java
+++ src/main/java/com/ajaguilar/apiRestful/repository/DailylogRepository.java
@@ -3,14 +3,24 @@ package com.ajaguilar.apiRestful.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ajaguilar.apiRestful.model.Dailylog;
-import com.ajaguilar.apiRestful.model.WorkerWork;
 import java.sql.Date;
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DailylogRepository extends JpaRepository<Dailylog, Long>{
 
+    /**
+     * Método que devuelve una lista de dailylogs por una fecha.
+     *
+     * @param fecha del dailylog.
+     * @return dailylogs con esa fecha.
+     */
+    @Query(value="SELECT d FROM dailylog d WHERE dailylog.date LIKE %?1", nativeQuery = true)
     List<Dailylog> findByDate(Date date);
-    List<Dailylog> findByWorkerwork(WorkerWork ww);
+    
+    
+    @Query(value="SELECT d FROM dailylog d JOIN dailylog.workerwork ON workerwork WHERE workerwork.id= ?1", nativeQuery = true)
+    List<Dailylog> findByWorkerwork(Long workerWorkId);
 }
