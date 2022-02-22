b50c63cfef36212315c1eb698c24f46cae859fe9
JuanFrancisco21 <juanfranciscoaguilarsanchez@gmail.com>
2022-02-22 11:10:19 +0100

    Dependencias para el despliege

---
+++ src/main/java/com/ajaguilar/apiRestful/ejecutable.java
@@ -0,0 +1,11 @@
+package com.ajaguilar.apiRestful;
+
+import com.ajaguilar.apiRestful.services.DriveService;
+
+public class ejecutable {
+
+	public static void main(String[] args) {
+		DriveService.uploadfile();
+	}
+
+}
